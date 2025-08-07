ed02c8e398bf99cec3078ef8b4ab504cc31fc4c2
sobhan <sobhan.amini.1381@gmail.com>
2025-08-07 11:44:49 +0330

    minor fixees

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
+use \App\Http\Controllers\Api\CallCenterController;
 
 Route::prefix('/products')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
